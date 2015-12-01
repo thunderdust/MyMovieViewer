413a355b2909aee3b263a37d9d9d2b3844257951
thunderdust <liuweiran.nus@gmail.com>
2015-12-01 15:49:58 +0800

    Add Related Movie Activity

---
+++ app/src/main/java/com/example/weiranliu/mymovieviewer/RelatedMovieActivity.java
@@ -0,0 +1,13 @@
+package com.example.weiranliu.mymovieviewer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RelatedMovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_related_movie);
+    }
+}
